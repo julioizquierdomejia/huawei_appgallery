6687227b43b05d3be7aa4b0ccd465854e4334cf7
Miguel AB <abm_8249@outlook.com>
2020-12-05 02:00:44 -0500

    botones en home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,15 +9,15 @@
 					<h1 class="title pt-md-4">Gana Exclusivas</h1>
 					<p class="text-uppercase">MasterClasses con <br>AppGallery y Claro Club</p>
 				</div>
-				<div class="col-12 bottom mt-auto pb-3">
-					<div class="buttons pr-2 pr-lg-5">
+				<div class="col-12 bottom mt-auto pb-5">
+					<div class="buttons pr-2 pr-lg-5 pb-5">
 						<div class="collapse" id="collapseLogin">
 							<form class="form-group f-group-w-icon" action="" method="POST">
 								<input type="text" name="email" class="form-control" placeholder="correo electrónico" required="">
 								<i class="fa fa-envelope icon"></i>
 							</form>
 						</div>
-						<button class="btn btn-red-transparent btn-login" type="button" data-toggle="collapse" data-target="#collapseLogin">Ingresar <i class="fa fa-play pl-2"></i></button>
+						<button class="btn btn-red-transparent btn-login mb-4" type="button" data-toggle="collapse" data-target="#collapseLogin">Ingresar <i class="fa fa-play pl-2"></i></button>
 						<button class="btn btn-danger btn-login mt-2" type="button" data-toggle="modal" data-target="#modalRegister">Regístrate <i class="fa fa-user pl-2"></i></button>
 					</div>
 				</div>
