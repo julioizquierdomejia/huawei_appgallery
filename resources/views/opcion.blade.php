396445256936bf11183cf9b417766592037ce1fa
Julio Izquierdo Mejia <julioizquierdomejia@Julios-MacBook-Pro.local>
2020-12-05 14:02:22 -0500

    textos

--- resources/views/opcion.blade.php
+++ resources/views/opcion.blade.php
@@ -36,8 +36,7 @@
 							<p class="text-above mb-1">Master Class</p>
 							<p class="pb-md-2">Entretenimiento Fullbody en casa con</p>
 							<h4 class="item-title mb-4">
-								<strong>Solange Barslund</strong>
-								<strong>"La Vikinga"</strong>
+								<strong>Solange Barslund <br>"La Vikinga"</strong>
 							</h4>
 							<button class="btn btn-danger" type="button" data-toggle="modal" data-target="#modalSoon">Ver vídeo</button>
 						</div>
