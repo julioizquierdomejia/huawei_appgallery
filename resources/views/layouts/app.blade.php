af8a131b3b214f73914c2620ff82ee19a5d0043d
Miguel AB <abm_8249@outlook.com>
2020-12-06 01:32:50 -0500

    cambiar id gtm

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,12 +35,12 @@
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-CY6XGKL35F');</script>
+    })(window,document,'script','dataLayer','GTM-KTBT37L');</script>
     <!-- End Google Tag Manager -->
   </head>
   <body class="page text-white {{isset($body_class) ? $body_class : ''}}">
     <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-CY6XGKL35F"
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KTBT37L"
     height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     <!-- End Google Tag Manager (noscript) -->
     <main class="main" style="display: flex;flex-direction: column;min-height: 100vh;">
